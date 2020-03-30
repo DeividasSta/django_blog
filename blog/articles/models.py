3ca5b243d439735ca4f419ac6ca8a1f586cd54a7
DeividasSta <48903757+DeividasSta@users.noreply.github.com>
2020-03-30 10:56:32 +0300

    creating article model

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    img_url = models.URLField(max_length=2083)
