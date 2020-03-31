0f45102422f1415c4026067cd76f0e385d4564e8
DeividasSta <48903757+DeividasSta@users.noreply.github.com>
2020-03-31 10:16:00 +0300

    creating url and template for showing each article

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -8,3 +8,10 @@ def index(request):
     context = {'articles': articles}
 
     return render(request, 'articles/index.html', context)
+
+
+def details(request, a_id):
+    article = Article.objects.get(pk=a_id)
+    context = {'article': article}
+
+    return render(request, 'articles/details.html', context)
